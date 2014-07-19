35f2c94dea8aa624bc2f91a62405347526bef3e9
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-07-19 23:54:03 +0430

    fixing

---
+++ languages/en/tl_kitchenware.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   Kitchenware
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_kitchenware']['title']     = array('Category title', 'Please enter the category title.');
+$GLOBALS['TL_LANG']['tl_kitchenware']['jumpTo']    = array('Redirect page', 'Please choose the product detail page to which visitors will be redirected when clicking a product item.');
+$GLOBALS['TL_LANG']['tl_kitchenware']['protected'] = array('Protect archive', 'Show category items to certain member groups only.');
+$GLOBALS['TL_LANG']['tl_kitchenware']['groups']    = array('Allowed member groups', 'These groups will be able to see the product items in this category.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_kitchenware']['title_legend']     = 'Title';
+$GLOBALS['TL_LANG']['tl_kitchenware']['redirect_legend']  = 'Redirect';
+$GLOBALS['TL_LANG']['tl_kitchenware']['protected_legend'] = 'Access protection';
+
+
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_kitchenware']['new']        = array('New Category', 'Create a new category');
+$GLOBALS['TL_LANG']['tl_kitchenware']['show']       = array('Category details', 'Show the details of category ID %s');
+$GLOBALS['TL_LANG']['tl_kitchenware']['edit']       = array('Edit Products', 'Edit products of category ID %s');
+$GLOBALS['TL_LANG']['tl_kitchenware']['editheader'] = array('Edit Category', 'Edit the setting of category ID %s');
+$GLOBALS['TL_LANG']['tl_kitchenware']['copy']       = array('Duplicate Category', 'Duplicate category ID %s');
+$GLOBALS['TL_LANG']['tl_kitchenware']['delete']     = array('Delete Category', 'Delete category ID %s');
