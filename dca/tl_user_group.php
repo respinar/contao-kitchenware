c0c96d5820355d7a2b29af89c1883f6b32395026
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-07-23 20:17:35 +0430

    adding permissions

---
+++ dca/tl_user_group.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package News
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Extend default palette
+ */
+$GLOBALS['TL_DCA']['tl_user_group']['palettes']['default'] = str_replace('fop;', 'fop;{kitchenware_legend},kitchenware,setp;', $GLOBALS['TL_DCA']['tl_user_group']['palettes']['default']);
+
+
+/**
+ * Add fields to tl_user_group
+ */
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['kitchenware'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['kitchenware'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'foreignKey'              => 'tl_kitchenware.title',
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
+
+$GLOBALS['TL_DCA']['tl_user_group']['fields']['setp'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_user']['setp'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'options'                 => array('create', 'delete'),
+	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'                    => array('multiple'=>true),
+	'sql'                     => "blob NULL"
+);
