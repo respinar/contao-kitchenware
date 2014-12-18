1fa85e42b9f3960beb7a300a7dcd891586134460
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2014-12-18 20:15:44 +0330

    adding tl_module.php

---
+++ languages/en/tl_module.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2013 Leo Feyer
+ *
+ * @package   kitchenware
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_categories']   = array('Kitchenware categories', 'Please select one or more kitchenware categories.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_featured']     = array('Featured set', 'Here you can choose how featured items are handled.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_price']        = array('Featured set', 'Here you can choose how featured items are handled.');
+
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_detailModule'] = array('Set detail module', 'Automatically switch to the set module if an item has been selected.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_metaFields']   = array('Meta fields', 'Here you can select the meta fields.');
+$GLOBALS['TL_LANG']['tl_module']['set_template']             = array('Set template', 'Here you can select the set template.');
+$GLOBALS['TL_LANG']['tl_module']['setClass']                 = array('Set CSS class', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['elementClass']             = array('Element CSS class', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['elementImageSize']         = array('Element image width and height', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_module']['colorClass']               = array('Color CSS class', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['colorImageSize']           = array('Color image width and height', 'Here you can set the image dimensions and the resize mode.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_module']['catalog_legend']   = 'Category';
+$GLOBALS['TL_LANG']['tl_module']['product_legend']   = 'Product setting';
+$GLOBALS['TL_LANG']['tl_module']['protected_legend'] = 'Access protection';
+$GLOBALS['TL_LANG']['tl_module']['type_legend']      = 'Type settings';
+
+
+/**
+ *
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['all_customers']       = 'All customers';
+$GLOBALS['TL_LANG']['tl_module']['feature_customers']   = 'Only feature customers';
+$GLOBALS['TL_LANG']['tl_module']['unfeature_customers'] = 'Unfeature customers';
