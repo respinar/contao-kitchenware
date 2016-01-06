326bbf6ada38424cb6a6d6ced296677b69542f8b
hamidabbaszadeh <abbaszadeh.h@gmail.com>
2016-01-06 19:26:57 +0330

    Update language strings

--- languages/en/tl_module.php
+++ languages/en/tl_module.php
@@ -21,27 +21,27 @@ $GLOBALS['TL_LANG']['tl_module']['kitchenware_sortBy']       = array('Sort By', 
 $GLOBALS['TL_LANG']['tl_module']['kitchenware_detailModule'] = array('Product detail module', 'Automatically switch to the set module if an item has been selected.');
 $GLOBALS['TL_LANG']['tl_module']['kitchenware_metaFields']   = array('Meta fields', 'Here you can select the meta fields.');
 
-$GLOBALS['TL_LANG']['tl_module']['product_template']         = array('Product template', 'Here you can select the set template.');
-$GLOBALS['TL_LANG']['tl_module']['product_Class']            = array('Product CSS class', 'Here you can set one or more classes for products.');
-$GLOBALS['TL_LANG']['tl_module']['product_perRow']           = array('Product per row', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_product_template']         = array('Product template', 'Here you can select the set template.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_product_Class']            = array('Product CSS class', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_product_perRow']           = array('Product per row', 'Here you can set one or more classes for products.');
 
-$GLOBALS['TL_LANG']['tl_module']['piece_show']               = array('Show pieces', 'Here you can set one or more classes for pieces.');
-$GLOBALS['TL_LANG']['tl_module']['piece_Class']              = array('Piece CSS class', 'Here you can set one or more classes for pieces.');
-$GLOBALS['TL_LANG']['tl_module']['piece_perRow']             = array('Piece per row', 'Here you can set one or more classes for pieces.');
-$GLOBALS['TL_LANG']['tl_module']['piece_imgSize']            = array('Piece image width and height', 'Here you can set the image dimensions and the resize mode.');
-$GLOBALS['TL_LANG']['tl_module']['piece_template']           = array('Piece template', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_piece_show']               = array('Show pieces', 'Here you can set one or more classes for pieces.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_piece_Class']              = array('Piece CSS class', 'Here you can set one or more classes for pieces.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_piece_perRow']             = array('Piece per row', 'Here you can set one or more classes for pieces.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_piece_imgSize']            = array('Piece image width and height', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_piece_template']           = array('Piece template', 'Here you can set the image dimensions and the resize mode.');
 
-$GLOBALS['TL_LANG']['tl_module']['type_show']                = array('Show types', 'Here you can set one or more classes for types.');
-$GLOBALS['TL_LANG']['tl_module']['type_Class']               = array('Type CSS class', 'Here you can set one or more classes for types.');
-$GLOBALS['TL_LANG']['tl_module']['type_imgSize']             = array('Type image width and height', 'Here you can set the image dimensions and the resize mode.');
-$GLOBALS['TL_LANG']['tl_module']['type_perRow']              = array('Type per row', 'Here you can set one or more classes for products.');
-$GLOBALS['TL_LANG']['tl_module']['type_template']            = array('Type template', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_type_show']                = array('Show types', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_type_Class']               = array('Type CSS class', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_type_imgSize']             = array('Type image width and height', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_type_perRow']              = array('Type per row', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_type_template']            = array('Type template', 'Here you can set one or more classes for products.');
 
-$GLOBALS['TL_LANG']['tl_module']['related_show']             = array('Show related products', 'Here you can set one or more classes for types.');
-$GLOBALS['TL_LANG']['tl_module']['related_Class']            = array('Related product CSS class', 'Here you can set one or more classes for types.');
-$GLOBALS['TL_LANG']['tl_module']['related_imgSize']          = array('Related product image width and height', 'Here you can set the image dimensions and the resize mode.');
-$GLOBALS['TL_LANG']['tl_module']['related_perRow']           = array('Related product per row', 'Here you can set one or more classes for products.');
-$GLOBALS['TL_LANG']['tl_module']['related_template']         = array('Related product template', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_related_show']             = array('Show related products', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_related_Class']            = array('Related product CSS class', 'Here you can set one or more classes for types.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_related_imgSize']          = array('Related product image width and height', 'Here you can set the image dimensions and the resize mode.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_related_perRow']           = array('Related product per row', 'Here you can set one or more classes for products.');
+$GLOBALS['TL_LANG']['tl_module']['kitchenware_related_template']         = array('Related product template', 'Here you can set one or more classes for products.');
 
 /**
  * Legends
